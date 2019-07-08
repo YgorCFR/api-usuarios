734bafcc33371df70177c84507e76f696be12bf0
ygor.rolim <ygor.rolim@radixeng.com.br>
2019-07-08 09:28:32 -0300

    Início na modificação da classe de teste de cobertura

---
+++ src/test/java/br/uff/qet/test/CadastroCobertura.java
@@ -0,0 +1,66 @@
+package br.uff.qet.test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import org.junit.Test;
+import org.springframework.http.ResponseEntity;
+
+import br.uff.qet.configuration.UserConfiguration;
+import br.uff.qet.controller.UserController;
+import br.uff.qet.model.User;
+import br.uff.qet.service.UserServiceImpl;
+
+
+public class CadastroCobertura {
+	
+	@Test
+	public void testarCadastro(){
+		//Declarando o serviço de gerenciamento de usuários
+		UserServiceImpl service = new UserServiceImpl();
+		
+		//Instanciando usuários
+		User usuarioPrimeiro = new User(2, "fizz@email.com", 78000, "Fizz", "ssds");
+		User usuarioSegundo = new User(3, "buzz@email.com", 1230, "Buzz", "eerapo");
+		
+		//Criando usuários
+		service.saveUser(usuarioPrimeiro);
+		service.saveUser(usuarioSegundo);
+		
+		//Verificando se os mesmos existem
+		service.isUserExist(usuarioPrimeiro);
+		service.isUserExist(usuarioSegundo);
+		
+		//Iniciando montagem de um ranking pelo email dos usuarios	
+		ArrayList<User> ranking = new ArrayList<User>();
+		ranking.add(service.findByEmailSenha(service.findById(2).getEmail(), service.findById(2).getSenha()));
+		ranking.add(service.findByEmailSenha(service.findById(1).getEmail(), service.findById(1).getSenha()));
+		ranking.add(service.findByEmailSenha(service.findById(3).getEmail(), service.findById(3).getSenha()));
+		
+		//Ordernando ranking
+		Collections.sort(ranking, Comparator
+				.comparing(User :: getPontos).reversed());
+		
+		for (User user : ranking) {
+			System.out.println(user.getEmail());
+		}
+		
+		ranking.get(ranking
+				.indexOf(service
+				.findByEmail(usuarioSegundo.getEmail())))
+				.setEmail("buzzbuzz@gmail.com");
+		
+		assertEquals("fizz@email.com", ranking.get(0).getEmail());
+	}
+	
+	//@Test
+	public void testarEdicoes() {
+	
+	}
+}
